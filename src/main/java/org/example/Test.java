2e6271e771c18c3d5e22fefd5198b6072f37bfb3
Shwetang <127761094+shwetang5@users.noreply.github.com>
2024-04-01 14:19:51 +0530

    Update Test.java

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         //made the changes
+        //mdae changes to master
     }
 }
