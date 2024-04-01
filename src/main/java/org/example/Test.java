2ec50adc5d03fd4dfa2870b71a72d9ba2eb5e6a7
Shwetang <127761094+shwetang5@users.noreply.github.com>
2024-04-01 14:22:18 +0530

    Feature/feature (#2)
    * changes made
    
    * Update main.java
    
    * changes made
    
    ---------
    
    Co-authored-by: shubhada93 <shubaha93@gmail.com>

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -5,6 +5,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         //made the changes
+        System.out.println("Hello World!123");
         //mdae changes to master
     }
 }
